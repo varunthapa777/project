574368f77294ff4d0a109c7e84396d9161f544d2
VarunThapa <140646101+varunthapa777@users.noreply.github.com>
2023-07-30 18:39:57 +0530

    Update example.py

--- .github/workflows/example.py
+++ .github/workflows/example.py
@@ -1,6 +1,12 @@
+import os
+
 def main():
   print("Hello From GitHub Actions!")
-
+  token = os.environ.get("MY_SECRET_TOKEN")
+  if not token:
+    raise RuntimeError("MY_SECRET_TOKEN env var is not set!")
+  print("All good! we found our env var!")
+  
 
 if __name__ == '__main__':
   main()
